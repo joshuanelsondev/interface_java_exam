54b87d160c00e68477b06122190ee20941e5ce27
joshuanelsondev <joshuanelsondev@gmail.com>
2025-05-01 08:12:19 -0400

    implement interface methods in Shape interface within the Circle class

--- src/main/java/org/example/Circle.java
+++ src/main/java/org/example/Circle.java
@@ -4,29 +4,31 @@ package org.example;
 public class Circle implements Shape {
 
     private final double radius;
+    private final double pi = 3.14159;
 
 
     public Circle(double radius) {
-  
+        if (radius <= 0) throw new IllegalArgumentException("Radius must be greater than zero.");
+        this.radius = radius;
     }
 
     public double getRadius() {
-  
+        return this.radius;
     }
 
     @Override
     public double calculateArea() {
-      
+        return pi * this.radius;
     }
 
     @Override
     public double calculatePerimeter() {
-     
+        return 2 * pi * this.radius;
     }
 
     @Override
     public String getType() {
-   
+        return "Circle";
     }
 
     @Override
