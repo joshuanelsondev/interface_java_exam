853447a52d43864314c0b7ca9f5af41f3f51553e
joshuanelsondev <joshuanelsondev@gmail.com>
2025-05-01 20:18:04 -0400

    implement methods within Rectangle class

--- src/main/java/org/example/Rectangle.java
+++ src/main/java/org/example/Rectangle.java
@@ -8,32 +8,37 @@ public class Rectangle implements Shape {
 
 
     public Rectangle(double width, double height) {
-  
+        this.width = width;
+        this.height = height;
     }
 
 
     public double getWidth() {
-     
+        return this.width;
     }
 
 
     public double getHeight() {
-     
+        return this.height;
     }
 
     @Override
     public double calculateArea() {
- 
+        return this.width * this.height;
     }
 
     @Override
     public double calculatePerimeter() {
-   
+        return (this.width * 2) + (this.height * 2);
     }
 
     @Override
     public String getType() {
-     
+        if ((this.calculateArea() / this.width) == this.height) {
+            return "Square";
+        }
+
+        return "Rectangle";
     }
 
     @Override
